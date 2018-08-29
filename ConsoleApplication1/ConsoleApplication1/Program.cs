3988942ceb65facbb57a08d386f31fab02aab545
Indra Chalavadhi <indra.chalavadhi@hexagon.com>
2018-08-29 12:29:49 +0530

    implemented feature1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApplication1
+﻿namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
             //initial modi from git
+            //featur1
         }
     }
 }
